0cf7b4d88118b3b6c1bb654ea3ee160dbb76b07c
Hann0T <hansolivas1@gmail.com>
2022-11-27 21:17:48 -0500

    refactor: first test.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
     run()?.await
 }
