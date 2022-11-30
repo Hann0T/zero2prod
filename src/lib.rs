d3a2a974aa33528eff9476cbfa9a5b25cff42d6e
Hann0T <hansolivas1@gmail.com>
2022-11-30 16:31:14 -0500

    new commit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,31 @@
-use actix_web::{web, App, HttpResponse, HttpServer, dev::Server};
+use std::net::TcpListener;
+
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Notice the different signature!
-// We return `Server` on the happy path and we dropped the `async` keyword 
+// We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
