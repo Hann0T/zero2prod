b4d039ee81c67e6cbb805eaeb2e969e86704444f
Hann0T <hansolivas1@gmail.com>
2022-11-30 19:56:22 -0500

    refactor: main.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
